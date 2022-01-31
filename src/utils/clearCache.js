64dd2114bd27d73ca58f6ca7444479cb75e6ec35
ASUS\Fraza <adityafraja12@gmail.com>
2022-01-31 11:57:38 +0700

    update versi

--- src/utils/clearCache.js
+++ src/utils/clearCache.js
@@ -2,7 +2,7 @@
 import Cookies from 'universal-cookie';
 
 // selalu update versiBuild + 1 agar otomatis hard refresh
-let versiBuild = 12
+let versiBuild = 13
 const cookies = new Cookies();
 
 export function SendVersiBuild() {
