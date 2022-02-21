1a3a2a979c7475176166d8ab31aa3d24cddc1ff3
ASUS\Fraza <adityafraja12@gmail.com>
2022-02-21 11:04:41 +0700

    add thumbnail FSAN

--- src/utils/clearCache.js
+++ src/utils/clearCache.js
@@ -2,7 +2,7 @@
 import Cookies from 'universal-cookie';
 
 // selalu update versiBuild + 1 agar otomatis hard refresh
-let versiBuild = 25
+let versiBuild = 26
 const cookies = new Cookies();
 
 export function SendVersiBuild() {
