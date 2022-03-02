efa8692fe6297ec35ad8ebceb5c2dc42931a0f63
rafly lesmana <raflylesmana111@gmail.com>
2022-03-02 15:26:18 +0700

    update version

--- src/utils/clearCache.js
+++ src/utils/clearCache.js
@@ -2,7 +2,7 @@
 import Cookies from 'universal-cookie';
 
 // selalu update versiBuild + 1 agar otomatis hard refresh
-let versiBuild = 33
+let versiBuild = 34
 const cookies = new Cookies();
 
 export function SendVersiBuild() {
