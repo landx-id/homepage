b146897a1bf8c5b2f67f7f64459ed56ef6a08920
rafly <rafly@landx.id>
2022-04-27 11:07:43 +0700

    update map atbs

--- src/utils/clearCache.js
+++ src/utils/clearCache.js
@@ -2,7 +2,7 @@
 import Cookies from 'universal-cookie';
 
 // selalu update versiBuild + 1 agar otomatis hard refresh
-let versiBuild = 51
+let versiBuild = 52
 const cookies = new Cookies();
 
 export function SendVersiBuild() {
