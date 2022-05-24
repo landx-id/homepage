72a76c5c4849fa68413732e419c4a8df77758ace
ASUS\Fraza <adityafraja12@gmail.com>
2022-05-24 09:35:16 +0700

    add kinerja penerbit v2

--- src/components/Card/CardKinerjaAssets/CardKinerjaAssets.js
+++ src/components/Card/CardKinerjaAssets/CardKinerjaAssets.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Card, CardContent, Typography } from '@mui/material'
+import './CardKinerjaAssets.scss'
 
-const CardKinerjaAssets = ({ cardDesc }) => {
+const CardKinerjaAssets = ({ cardDesc, cardActive }) => {
   return (
     <>
-      <Card>
+      <Card className={`utils cursor-pointe card-kinerja-asset ${cardActive ? 'active' : ''}`}>
         <CardContent>
           <Typography className="utils flex center-start font-17 line-height-24 color-black-700">
             <img src="/assets/icons/checkbox-green.svg" alt="checkbox" className="utils mr-18" /> {cardDesc}
