b6d1754c643a9dbfd2b766b7bc6ace2d7b0bcf1c
yolanda ihsan <coxskazoku@gmail.com>
2022-07-26 08:47:01 +0700

    add cs 3 phone number

--- src/components/HeaderAlerts/HeaderAlerts.js
+++ src/components/HeaderAlerts/HeaderAlerts.js
@@ -26,17 +26,38 @@ const HeaderAlerts = () => {
 
   const handleAlert = (windowDimension) => {
     if (windowDimension > 1336) {
-      return <span style={{ display: 'flex', alignItems: 'center' }}>Hati-hati dengan segala jenis penipuan. LandX tidak memiliki grup Telegram! Akun resmi Customer Service LandX adalah <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0811 9562 878 dan <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0813 813 8186 2878</span>
+      return <span style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        Hati-hati dengan segala jenis penipuan. LandX tidak memiliki grup Telegram! Akun resmi Customer Service LandX adalah
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'row', justifyContent: 'center', gap: '4px' }}>
+          <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0811 9562 878,
+          <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0813 813 8186 2878, dan
+          <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0811 1622 878 (Consultant Representative)
+        </div >
+      </span>
     }
     else {
-      return <span style={{ display: 'flex', alignItems: 'center' }}><marquee>Hati-hati dengan segala jenis penipuan. LandX tidak memiliki grup Telegram! Akun resmi Customer Service LandX adalah <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0811 9562 878 dan <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span> 0813 813 8186 2878</marquee></span>
+      return (
+        <span style={{ display: 'flex', alignItems: 'center' }}>
+          <marquee>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              Hati-hati dengan segala jenis penipuan. LandX tidak memiliki grup Telegram! Akun resmi Customer Service LandX adalah
+              <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span>
+              0811 9562 878,
+              <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span>
+              0813 813 8186 2878, dan
+              <span style={{ margin: '3px 5px 0' }}><WhatsAppIcon /></span>
+              0811 1622 878 (Consultant Representative)
+            </div>
+          </marquee>
+        </span>
+      )
     }
   }
 
   return (
     <>
       <Collapse in={open}>
-        <Alert sx={{ height: '50px', alignItems: 'center', fontSize: '16px', justifyContent: 'center' }} variant="filled" severity="info">
+        <Alert sx={{ alignItems: 'center', fontSize: '16px', justifyContent: 'center', padding: '8px' }} variant="filled" severity="info" icon={false}>
           {handleAlert(windowDimension)}
         </Alert>
       </Collapse>
